01c2d60e16684a8194fffb338ca0009158049096
Ashin <ashin.jnv@gmail.com>
2023-06-18 11:56:05 +0530

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,30 +5,43 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
   const { register, handleSubmit } = useForm(); //useFrom
   const [data, setData] = useState(""); // using state to create a variable named data that is initially a null string
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     //useForm submit function that collects data and store it in "data variable" create by useState above
     setData(data); //setting varibale "data's" value to the form values
     console.log(data); // consoling the form values , which is in json format
+
+    //! sending to backend
+    //we are using a npm package called axios. install using npm i axios in frontend folder
+
+    await sendData(data);
   };
 
+  const sendData = async (data) => {
+    try {
+      const send = await axios.post("http://localhost:4578/addData", data);
+      console.log(send);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <h1 className="text-center mt-3 ">
         Data Insert from frontend to Mongodb through backend
       </h1>
 
-
-{/* when form gets submitted a function "handleSubmit" gets activates to trigger "onSubmit" function */}
+      {/* when form gets submitted a function "handleSubmit" gets activates to trigger "onSubmit" function */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="">Title</label>
 
         {/* ...register ("field_name") means to collect the data/value inserted by the user through input field and register it in json */}
-        <input placeholder="Title" {...register("title")} /> 
+        <input placeholder="Title" {...register("title")} />
 
         <label htmlFor="">Price</label>
         <input placeholder="Price" {...register("price")} />
