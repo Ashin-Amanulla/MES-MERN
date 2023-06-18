8ed5762ccdb269ae3dd27528a893903796b49ccb
Ashin <ashin.jnv@gmail.com>
2023-06-18 14:25:28 +0530

    dasd

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose'); //mongoose for DB
 const cors = require('cors'); // to remore cor issue
 app.use(cors())  // cor policy activation
 app.use(express.json()); // to render json req from frontend
-app.use(express.urlencoded({extended:true})); // to render form data from frontend
+app.use(express.urlencoded({ extended: true })); // to render form data from frontend
 
 //connect backend with DB
-mongoose.connect("mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/")//!insert your username
+mongoose.connect("mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/MES")//!insert your username
     .then(() => { console.log("MongoDB connected successfully") })//*success message
     .catch((err) => { console.log("Error connecting to MongoDB " + err) });//*fail message
 
@@ -18,12 +18,19 @@ mongoose.connect("mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/")//!
 //U-Update -PUT
 //D-Delete - DELETE
 
-app.post('/addData', (req, res) => {
+
+const PRODUCT = require("./model/product")
+
+app.post('/addData',async (req, res) => {
     try {
 
         let item = req.body
         console.log(item)
 
+
+        const saveData = await PRODUCT(item)
+        await saveData.save()
+        res.send((saveData))
 
 
     } catch (error) {
