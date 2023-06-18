56db29e91794d8fd38d7a64c96a655db2647cbc6
Ashin <ashin.jnv@gmail.com>
2023-06-18 12:26:40 +0530

    sdas

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = 4578 //PORT
 const mongoose = require('mongoose'); //mongoose for DB
+const cors = require('cors'); // to remore cor issue
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true})); // to render form data from frontend
 
 //connect backend with DB
 mongoose.connect("mongodb+srv://userone:userone@cluster0.vcc0q.mongodb.net/")//!insert your username
-.then(()=>{console.log("MongoDB connected successfully")})//*success message
-.catch((err)=>{console.log("Error connecting to MongoDB " + err)});//*fail message
+    .then(() => { console.log("MongoDB connected successfully") })//*success message
+    .catch((err) => { console.log("Error connecting to MongoDB " + err) });//*fail message
+
+// CRUD operation 
+// C-Create - POST 
+//R-READ-GET
+//U-Update -PUT
+//D-Delete - DELETE
+
+app.post('/addData', (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Server is Ashifdsfdsn')
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
